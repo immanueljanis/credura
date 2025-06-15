3223b470d400fefc73266ce176b33eaa07c30eea
Rakemoon <rakemoonkuzmin@gmail.com>
2025-06-15 11:58:29 +0700

    chore: add abi

--- frontend/src/app/profile/enrol-dialog.tsx
+++ frontend/src/app/profile/enrol-dialog.tsx
@@ -1,15 +1,19 @@
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { useState } from "react";
 
 export function EnrolDialog() {
   const [open, setOpen] = useState(false);
   return (
-    <>
-      <button
+    <Dialog>
+      <DialogTrigger
         onClick={() => setOpen(true)}
         className="btn-primary flex items-center text-sm"
       >
         Enroll Now
-      </button>
-    </>
+      </DialogTrigger>
+      <DialogContent>
+        djskdj
+      </DialogContent>
+    </Dialog>
   );
 }
